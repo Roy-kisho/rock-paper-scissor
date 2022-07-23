cc4d965971f34cb696f0d92f69845f2ed7ddcbe3
Roy-kisho <roykisho9@gmail.com>
2022-07-23 13:28:10 +0530

    Fully modified

--- main.py
+++ main.py
@@ -1,66 +1,93 @@
 import random
 
+
 # points
 user_points = 0
 computer_points = 0
 
+while user_points != 3 and computer_points != 3:
 
+    # Computer choice
+    random_num = random.randint(1, 3)
+    computer_choice = ""
 
-while computer_points != 3 or user_points != 3:
-    # Computer choice 
-    computer_choice = random.randint(1, 3);
-    rock_paper_or_scissor = ""
     # checking whether computer choosed rock paper or scissor
-    if computer_choice == 1:
-        rock_paper_or_scissor = "rock"
-    elif computer_choice == 2:
-        rock_paper_or_scissor = "paper"
-    elif computer_choice == 3:
-        rock_paper_or_scissor = "scissor"
-    
+    if random_num == 1:
+        computer_choice = "r"
+    elif random_num == 2:
+        computer_choice = "p"
+    elif random_num == 3:
+        computer_choice = "s"
+
     # User choice
-    user_choice = input("Enter Rock,Paper,Scissor to play or q to quit: ").lower()
-    print(f"You played {user_choice}")
-    print(f"Computer played {rock_paper_or_scissor}")
+    user_choice = input(
+        "Enter (r)ock,(p)aper, (s)cissor to play or q to quit: ").lower()
 
-    # Game functions
+    if user_choice == 'r' or user_choice == 's' or user_choice == 'p' or user_choice == 'q':
+        # Game functions
 
-    # When the game is tied
-    if user_choice == "rock" and computer_choice == 1:
-        print("Draw game try again")
-    elif user_choice == "paper" and computer_choice == 2:
-        print("Draw game try again")
-    elif user_choice == "scissor" and computer_choice == 3:
-        print("Draw game try again")
-        
-    # When user wins 
-    elif user_choice == "rock" and computer_choice == 3:
-        print("Congrats You won :)")
-        user_points += 1
-    elif user_choice == "paper" and computer_choice == 1:
-        print("Congrats You won :)")
-        user_points += 1
-    elif user_choice == "scissor" and computer_choice == 2:
-        print("Congrats You won :)")
-        user_points += 1
+        # When the game is tied
+        if user_choice == computer_choice:
+            if user_choice == 'r':
+                print("Rock vs Rock")
+            elif user_choice == 'p':
+                print("Paper vs Paper")
+            elif user_choice == 's':
+                print("Scissor vs Scissor")
+            print("Game Draw 😬")
+            print(f"Player points : {user_points}")
+            print(f"Ai points : {computer_points}")
+        # When user wins
+        elif user_choice == "r" and computer_choice == 's':
+            print("Rock Vs Scissor")
+            print("You won 😉")
+            user_points += 1
+            print(f"Player points : {user_points}")
+            print(f"Ai points : {computer_points}")
+        elif user_choice == "p" and computer_choice == 'r':
+            print("Paper Vs Rock")
+            print("You won 😉")
+            user_points += 1
+            print(f"Player points : {user_points}")
+            print(f"Ai points : {computer_points}")
+        elif user_choice == "s" and computer_choice == 'p':
+            print("Scissor Vs Paper")
+            print("You won 😉")
+            user_points += 1
+            print(f"Player points : {user_points}")
+            print(f"Ai points : {computer_points}")
 
-    # When computer wins
-    elif computer_choice == 1 and user_choice == "scissor":
-        print("Oops Computer wins")
-        computer_choice += 1
-    elif computer_choice == 2 and user_choice == "rock":
-        print("Oops Computer wins")
-        computer_choice += 1
-    elif computer_choice == 3 and user_choice == "paper":
-        print("Oops Computer wins")
-        computer_choice += 1
-    elif user_choice == "q":
-        print("Thank you for playing, come and play again :)")
-        break
-    else :
-        print("You entered something behalf of Rock,Paper and Scissor :(")
+        # When computer wins
+        elif user_choice == "s" and computer_choice == 'r':
+            print("Scissor Vs Rock")
+            print("You lose 😕")
+            computer_points += 1
+            print(f"Player points : {user_points}")
+            print(f"Ai points : {computer_points}")
+        elif user_choice == "r" and computer_choice == 'p':
+            print("Rock Vs Paper")
+            print("You lose 😕")
+            computer_points += 1
+            print(f"Player points : {user_points}")
+            print(f"Ai points : {computer_points}")
+        elif user_choice == "p" and computer_choice == 's':
+            print("Paper Vs Scissor")
+            print("You lose 😕")
+            computer_points += 1
+            print(f"Player points : {user_points}")
+            print(f"Ai points : {computer_points}")
+        elif user_choice == "q":
+            print(f"Player points : {user_points}")
+            print(f"Ai points : {computer_points}")
+            break
+    else:
+        print("""
+r -> Rock
+p -> paper
+s -> Scissor
+""")
 
-
-
-print(f"User total points : {user_points}")
-print(f"Computer total points : {computer_points}")
+if user_points > computer_points:
+    print("Awesome you won the game 🕺")
+elif computer_points > user_points:
+    print("Oops you lost the game, better luck next time 🙂.")
